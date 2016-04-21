09460b9148127076a02632feefa62ba3d1fc4912
Nahuel <nahuelj91@gmail.com>
2016-04-21 16:10:20 -0300

    catckin_create_pkg

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------
+//
+//  Name:   main.cpp
+//
+//  Desc:   Instancia los controladores para la cantidad de robots
+//          existentes en el mundo del simulador
+//
+//  Author: Nahuel Jose 2016 (nahuelj91@gmail.com)
+//
+//------------------------------------------------------------------------
