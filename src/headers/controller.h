3f914482fbee5e9cd100a611e93f49fa8c5434a2
Nahuel <nahuelj91@gmail.com>
2016-04-21 17:58:50 -0300

    controller.h

---
+++ src/headers/controller.h
@@ -0,0 +1,29 @@
+#ifndef controller_H
+#define controller_H
+
+//------------------------------------------------------------------------
+//
+//  Name:   controller.h
+//
+//  Desc:   Recaba los aportes de los comportamientos, calcula la resultante
+// 			y actúa sobre el agente
+//
+//  Author: Nahuel Jose 2016 (nahuelj91@gmail.com)
+//
+//------------------------------------------------------------------------
+
+
+class Controller
+{
+	private:
+
+	public:
+		Controller();
+
+		~Controller();
+
+		void	Update();
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,42 @@
 //  Author: Nahuel Jose 2016 (nahuelj91@gmail.com)
 //
 //------------------------------------------------------------------------
+
+#include "ros/ros.h"
+#include "headers/controller.h"
+
+int main(int argc, char const *argv[])
+{
+	int robots;
+	int behaviors[robots]; //representar en binario
+	controller* ctrls[robots];
+
+	//obtener la cantidad de robots
+	robots = 4; //=algo con rostopic list o enviarlo por los arg con el nro de robot o con que intervalo de robots
+
+	//definir los comportamientos para los robots
+	for (int i = 0; i < robots; ++i)
+	{
+		behaviors[i] = 15; //para evaluar en binario, se podria tomar de los argv
+	}
+
+	//instanciar los controladores
+	for (int i = 0; i < robots; ++i)
+	{
+		ctrls[i] = controller(i,behaviors); //intanciar el control para cada robot. Arg: i para saber en que topic publicar y behaviors que representa los comport a activar en el ctrl
+	}
+
+	//rutina de trabajo
+	while(ros::ok()){
+		//actualizar cada controlador, analizar el entorno por cada behavior, sumar, ponderar y actualizar la actuacion
+		for (int i = 0; i < robots; ++i)
+		{
+			ctrls[i].actualize();
+		}
+
+		ros::spinOnce();
+		loop_rate.sleep(); ///sleep por el resto del ciclo
+	}
+
+	return 0;
+}
